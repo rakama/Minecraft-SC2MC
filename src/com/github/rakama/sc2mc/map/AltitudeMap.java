208ff8be421948cc244245827d658a583dbfbb49
RamsesA <ramsesakama@gmail.com>
2012-08-06 04:50:53 -0400

    Adjusted coastlines
    Coastlines now more closely resemble SimCity 2000

--- src/com/github/rakama/sc2mc/map/AltitudeMap.java
+++ src/com/github/rakama/sc2mc/map/AltitudeMap.java
@@ -1,5 +1,21 @@
 package com.github.rakama.sc2mc.map;
 
+/**
+ * Copyright (c) 2012, RamsesA <ramsesakama@gmail.com>
+ * 
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+ * REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+ * AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+ * INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+ * LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+ * OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ * PERFORMANCE OF THIS SOFTWARE.
+ */
+
 public class AltitudeMap
 {
     public static final int width = 128;
@@ -28,13 +44,15 @@ public class AltitudeMap
     int[] water;
     Slope[] slope;
     Rotation[] rotation;
+    boolean[] underwater;
     
-    public AltitudeMap(byte[] altm, byte[] xter)
+    protected AltitudeMap(byte[] altm, byte[] xter)
     {
         terrain = new int[width * height];
         water = new int[width * height];
         slope = new Slope[width * height];
         rotation = new Rotation[width * height];
+        underwater = new boolean[width * height];
         
         for(int x=0; x<width; x++)
         {
@@ -46,6 +64,7 @@ public class AltitudeMap
                 water[index] =  getWaterAltitude(altm, x, y);                
                 slope[index] = getSlopeType(xter, x, y);            
                 rotation[index] = getSlopeRotation(xter, x, y);
+                underwater[index] = getUnderwater(xter, x, y);
             }
         }
     }
@@ -107,18 +126,42 @@ public class AltitudeMap
         case SLOPE:
             return altitude + 1 - yf;
         case CORNER_LOW:
-            if(yf > xf)
-                return altitude + 1 - yf + xf;
+            if(underwater[index])
+                return altitude + getQuadraticCornerLow(xf, yf);
+            else if(yf > xf)
+                return altitude + getCornerLow(xf, yf);
             else
                 return altitude + 1;
         case CORNER_HIGH:
-            if(yf < xf)
-                return altitude - yf + xf;
+            if(underwater[index])
+                return altitude + getQuadraticCornerHigh(xf, yf);
+            else if(yf < xf)
+                return altitude + getCornerHigh(xf, yf);
             else
                 return altitude;
         }
         
         return altitude;
+    }
+
+    protected float getCornerLow(float xf, float yf)
+    {
+        return 1 - yf + xf;
+    }
+
+    protected float getCornerHigh(float xf, float yf)
+    {
+        return xf - yf;
+    }
+    
+    protected float getQuadraticCornerLow(float xf, float yf)
+    {
+        return (float)Math.min(1, (1 - xf) * (1 - yf) + xf * (1 - yf) + xf * yf + 0.05);
+    }
+
+    protected float getQuadraticCornerHigh(float xf, float yf)
+    {
+        return (float)Math.min(1, (1 - yf) * xf + 0.05);
     }
     
     public int getWaterAltitude(int x, int y)
@@ -139,6 +182,12 @@ public class AltitudeMap
         return xterRotation[0xF & data[index]];        
     }
 
+    protected static boolean getUnderwater(byte[] data, int x, int y)
+    {
+        int index = (127 - x) + (y << 7);
+        return (data[index] & 0x30) != 0;        
+    }
+
     protected static int getTerrainAltitude(byte[] data, int x, int y)
     {
         int index = ((127 - x) + (y << 7)) << 1;
@@ -148,7 +197,9 @@ public class AltitudeMap
     protected static int getWaterAltitude(byte[] data, int x, int y)
     {
         int index = ((127 - x) + (y << 7)) << 1;
-        int val = data[index + 1] & 0xFF;
+        int byte1 = data[index] & 0xFF;
+        int byte2 = data[index + 1] & 0xFF;
+        int val = byte2 | (byte1 << 8);
         return (val >> 5) & 0xF;
     }
         
